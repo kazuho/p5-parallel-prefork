214caac130d24a99e60b2e183ea32b8874b706cb
kazuho <kazuho@d0d07461-0603-4401-acd4-de1884942a52>
2010-03-28 13:51:55 +0000

    releng 0.08
    git-svn-id: http://svn.coderepos.org/share/lang/perl/Parallel-Prefork/trunk@37110 d0d07461-0603-4401-acd4-de1884942a52

--- lib/Parallel/Prefork.pm
+++ lib/Parallel/Prefork.pm
@@ -9,7 +9,7 @@ use Proc::Wait3;
 
 __PACKAGE__->mk_accessors(qw/max_workers err_respawn_interval trap_signals signal_received manager_pid on_child_reap/);
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 sub new {
     my ($klass, $opts) = @_;
