8f67f0d38644b795a8cde2640f5758031407c0fa
Toru Yamaguchi <yamaguchi.toru@51547-mac.dena.local>
2011-10-19 21:17:51 +0900

    Supports before_fork, after_fork callback

--- lib/Parallel/Prefork.pm
+++ lib/Parallel/Prefork.pm
@@ -14,7 +14,7 @@ use Class::Accessor::Lite (
     rw => [ qw/max_workers spawn_interval err_respawn_interval trap_signals signal_received manager_pid on_child_reap/ ],
 );
 
-our $VERSION = '0.11';
+our $VERSION = '0.12';
 
 sub new {
     my $klass = shift;
